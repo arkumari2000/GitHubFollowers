9638fcae854d81407c8e4d1e410196ae4f55d3b6
Archana Kumari <arkumari2000@gmail.com>
2025-07-21 20:28:23 +0530

    Initial Commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Himanshu Bhati on 21/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GitHubFollowersTests/GitHubFollowersTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubFollowersTests.swift
+//  GitHubFollowersTests
+//
+//  Created by Himanshu Bhati on 21/07/25.
+//
+
+import Testing
+@testable import GitHubFollowers
+
+struct GitHubFollowersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
