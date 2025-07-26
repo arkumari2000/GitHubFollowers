52869906c913e33af1989b7bfdee19d8dc586d4c
Archana Kumari <arkumari2000@gmail.com>
2025-07-26 18:47:51 +0530

    Added Data Models

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Himanshu Bhati on 26/07/25.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GitHubFollowers/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GitHubFollowers
+//
+//  Created by Himanshu Bhati on 26/07/25.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
