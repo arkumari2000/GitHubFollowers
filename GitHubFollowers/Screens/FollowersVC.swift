93f8e7b785b1d920c6ecc88724d61a4fa264e6e1
Archana Kumari <arkumari2000@gmail.com>
2025-07-30 20:48:12 +0530

    Refractored Network Manager

--- GitHubFollowers/Screens/FollowersVC.swift
+++ GitHubFollowers/Screens/FollowersVC.swift
@@ -16,14 +16,18 @@ class FollowersVC: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        NetworkManager.shared.getFollowers(for: userName, page: 1) { (followers, errorMessage) in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: errorMessage?.rawValue, buttonTitle: "Ok")
+        NetworkManager.shared.getFollowers(for: userName, page: 1) { result in
+            
+            switch result {
+            case .success(let followers):
+                print("Followers Count: \(followers.count)")
+                print(followers)
+                
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
                 return
             }
             
-            print("Followers Count: \(followers.count)")
-            print(followers)
         }
     }
     
