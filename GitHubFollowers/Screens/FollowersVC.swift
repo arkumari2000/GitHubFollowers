0349d6ced1d8eea33afb0096a5fc001174e7fc71
Archana Kumari <arkumari2000@gmail.com>
2025-08-01 19:33:33 +0530

    Created Custom Flowlayout

--- GitHubFollowers/Screens/FollowersVC.swift
+++ GitHubFollowers/Screens/FollowersVC.swift
@@ -45,8 +45,22 @@ class FollowersVC: UIViewController {
         }
     }
     
+    func createThreeColumnLayoutFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.frame.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+    
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.frame, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView = UICollectionView(frame: view.frame, collectionViewLayout: createThreeColumnLayoutFlowLayout())
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemPink
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseId)
