17381a7c82f9676d6c03bd592127fdbfba3f000c
Archana Kumari <arkumari2000@gmail.com>
2025-09-02 10:20:20 +0530

    Added dismiss button in UserInfoVC

--- GitHubFollowers/Screens/UserInfoVC.swift
+++ GitHubFollowers/Screens/UserInfoVC.swift
@@ -12,7 +12,16 @@ class UserInfoVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemCyan
+        view.backgroundColor = .systemBackground
+        configureDoneButton()
+    }
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
     }
 
+    func configureDoneButton() {
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+    }
 }
