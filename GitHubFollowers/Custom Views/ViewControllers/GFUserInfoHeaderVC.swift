44a224b88db5acdc4854df21169921a202897afa
Archana Kumari <arkumari2000@gmail.com>
2025-09-05 22:05:11 +0530

    Added secondary title label

---
+++ GitHubFollowers/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
@@ -0,0 +1,16 @@
+//
+//  GFUserInfoHeaderVC.swift
+//  GitHubFollowers
+//
+//  Created by Archana Kumari on 05/09/25.
+//
+
+import UIKit
+
+class GFUserInfoHeaderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
